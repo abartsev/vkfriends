3c1550e8d2050ce57e2195e77e997fd296a3cfbd
ABarcev <a.barczev@yandex.ru>
2018-08-23 19:42:40 +0300

    edit2

--- script.js
+++ script.js
@@ -15,4 +15,32 @@ function auth() {
     });
     
 }
-auth().then(() => console.log('ok'));
+function callAPI(method, params) {
+    params.v = '5.76';
+
+    return new Promise((resolve, reject) => {
+            VK.api(method, params, (data) => {
+                if (data.Error) {
+                    reject(data.Error);
+                } else {
+                    resolve(data.response);
+                }
+            })
+    })
+    
+}
+auth()
+.then(() => {
+    return callAPI('users.get', {name_case: 'gen'});
+})
+.then(([me]) => {
+    const headerInfo = document.querySelector('#headerInfo');
+    headerInfo.textContent - `Друзья на странице ${me.first_name} ${me.last_name}`;
+
+    return callAPI('friends.get', {fields: 'city, country, photo_100'});
+    
+})
+.then(friends => {
+    console.log(friends);
+    
+});
